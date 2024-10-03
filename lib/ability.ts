59f4bdfb7c1875b0f54a72ce47c41dfc7ad7c853
Abdisa Dev <AbdisaDev@gmail.com>
2024-10-03 10:11:06 +0300

    final bug

--- lib/ability.ts
+++ lib/ability.ts
@@ -1,33 +1,29 @@
-import {
-  PureAbility,
-  AbilityBuilder,
-  InferSubjects,
-  MatchConditions,
-} from "@casl/ability";
+import { PureAbility, AbilityBuilder, InferSubjects } from '@casl/ability';
 
-export type Actions = "manage" | "create" | "read" | "update" | "delete";
+export type Actions = 'manage' | 'create' | 'read' | 'update' | 'delete';
 export type Subjects = InferSubjects<
-  "Order" | "User" | "Role" | "Menu" | "all"
+  'Order' | 'User' | 'Role' | 'Menu' | 'all'
 >;
 export type AppAbility = PureAbility<[Actions, Subjects]>;
 
-const lambdaMatcher = (matchConditions: MatchConditions) => matchConditions;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const lambdaMatcher = (matchConditions: any) => matchConditions;
 
 export function defineAbilityFor(user): AppAbility {
   const { can, cannot, build } = new AbilityBuilder<
     PureAbility<[Actions, Subjects]>
   >(PureAbility);
 
-  if (user?.role === "superadmin") {
-    can("manage", "all");
-  } else if (user?.role === "manager") {
-    can("manage", "all");
-    cannot("delete", "all");
+  if (user?.role === 'superadmin') {
+    can('manage', 'all');
+  } else if (user?.role === 'manager') {
+    can('manage', 'all');
+    cannot('delete', 'all');
   } else {
-    can("create", "Order");
-    can("read", "Order", { id: user?.id });
-    cannot("update", "Order");
-    cannot("delete", "all");
+    can('create', 'Order');
+    can('read', 'Order', { id: user?.id });
+    cannot('update', 'Order');
+    cannot('delete', 'all');
   }
 
   return build({ conditionsMatcher: lambdaMatcher });
