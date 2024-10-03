8620edc7f044586ce067f29db590c209e17b7854
Abdisa Dev <abdisadev@gmail.com>
2024-10-03 06:10:55 +0300

    user issue fixed

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,73 +4,73 @@ import {
   InputAdornment,
   TextField,
   Typography,
-} from '@mui/material';
-import Link from 'next/link';
-import Image from 'next/image';
-import Logo from '@/app/assets/logo.svg';
+} from "@mui/material";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/app/assets/logo.svg";
 import {
   Facebook,
   LinkedIn,
   Send as SendIcon,
   Twitter,
-} from '@mui/icons-material';
+} from "@mui/icons-material";
 
 function Footer() {
   return (
     <Box>
       <Box
         sx={{
-          height: '200px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          background: '#CCB691',
+          height: "200px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          background: "#CCB691",
           mt: 10,
         }}
       >
         <Box
           sx={{
-            width: '30%',
-            display: 'flex',
-            justifyContent: 'space-around',
-            fontWeight: 'bolder',
-            fontSize: '30px',
+            width: "30%",
+            display: "flex",
+            justifyContent: "space-around",
+            fontWeight: "bolder",
+            fontSize: "30px",
           }}
         >
-          <Link href='#'>Order</Link>
-          <Link href='#'>About Us</Link>
-          <Link href='#'>Home</Link>
+          <Link href="/order">Order</Link>
+          <Link href="#">About Us</Link>
+          <Link href="/">Home</Link>
         </Box>
         <Box
           sx={{
-            width: '30%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            mr: '50px',
+            width: "30%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            mr: "50px",
           }}
         >
-          <Image src={Logo} priority={true} alt='logo' />
+          <Image src={Logo} priority={true} alt="logo" />
           <TextField
-            placeholder='Your Feedback'
+            placeholder="Your Feedback"
             fullWidth
             slotProps={{
               input: {
                 sx: {
-                  borderRadius: '10px',
-                  background: '#fff',
-                  border: 'none',
-                  p: '0 20px',
-                  my: '20px',
+                  borderRadius: "10px",
+                  background: "#fff",
+                  border: "none",
+                  p: "0 20px",
+                  my: "20px",
                 },
                 endAdornment: (
-                  <InputAdornment position='end'>
+                  <InputAdornment position="end">
                     <IconButton
-                      edge='end'
+                      edge="end"
                       sx={{
-                        background: '#FA8910',
-                        color: '#fff',
-                        p: 'fit-content',
+                        background: "#FA8910",
+                        color: "#fff",
+                        p: "fit-content",
                       }}
                     >
                       <SendIcon />
@@ -84,28 +84,28 @@ function Footer() {
       </Box>
       <Box
         sx={{
-          height: 'fit-content',
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          background: '#000',
-          color: '#eee',
-          p: '10px 50px',
+          height: "fit-content",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          background: "#000",
+          color: "#eee",
+          p: "10px 50px",
         }}
       >
-        <Box sx={{ display: 'flex', columnGap: 3 }}>
+        <Box sx={{ display: "flex", columnGap: 3 }}>
           <Typography>@2024 Pizza All Rights Reserved.</Typography>
-          <Link href='#'>Terms and Conditions</Link>
+          <Link href="#">Terms and Conditions</Link>
         </Box>
         <Box>
-          <IconButton size='large' color='secondary'>
-            <Facebook htmlColor='#eee' fontSize='large' />
+          <IconButton size="large" color="secondary">
+            <Facebook htmlColor="#eee" fontSize="large" />
           </IconButton>
-          <IconButton size='large'>
-            <LinkedIn fontSize='large' htmlColor='#eee' />
+          <IconButton size="large">
+            <LinkedIn fontSize="large" htmlColor="#eee" />
           </IconButton>
-          <IconButton size='large'>
-            <Twitter fontSize='large' htmlColor='#eee' />
+          <IconButton size="large">
+            <Twitter fontSize="large" htmlColor="#eee" />
           </IconButton>
         </Box>
       </Box>
