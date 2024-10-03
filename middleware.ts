d82a31a0700764d57812d13b9dee8ca83a759ebd
Abdisa Dev <AbdisaDev@gmail.com>
2024-10-03 12:37:44 +0300

    auth

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { getToken } from 'next-auth/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = req.nextUrl;
 
   if (
