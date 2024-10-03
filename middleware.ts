cea3465a603d759ad070de3848c2b31a54fa7534
Abdisa Dev <AbdisaDev@gmail.com>
2024-10-03 14:44:07 +0300

    next sec

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,31 @@
-// import { getToken } from 'next-auth/jwt';
-// import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
+import { auth } from './auth';
 
-// export async function middleware(req: NextRequest) {
-//   const token = await getToken({ req });
-//   const { pathname } = req.nextUrl;
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+  const { pathname } = req.nextUrl;
 
-//   if (
-//     (pathname.startsWith('/dashboard') && !token) ||
-//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//     (token?.user as any)?.type === 'CUSTOMER'
-//   ) {
-//     const loginUrl = new URL('/', req.url);
+  if (
+    (pathname.startsWith('/dashboard') && !token) ||
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (token?.user as any)?.type === 'CUSTOMER'
+  ) {
+    const loginUrl = new URL('/', req.url);
 
-//     return NextResponse.redirect(loginUrl);
-//   }
-//   if (pathname.startsWith('/auth')) {
-//     const loginUrl = new URL('/auth/login', req.url);
+    return NextResponse.redirect(loginUrl);
+  }
+  if (pathname.startsWith('/auth')) {
+    const loginUrl = new URL('/auth/login', req.url);
 
-//     return NextResponse.redirect(loginUrl);
-//   }
+    return NextResponse.redirect(loginUrl);
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: ['/dashboard/:path*', '/dashboard'],
-// };
+export const config = {
+  matcher: ['/dashboard/:path*', '/dashboard'],
+};
+
+export default auth;
